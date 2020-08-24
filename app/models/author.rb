29acfc973e2af5dde6f06f10b33ed82463783576
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-08-24 16:08:19 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,14 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+
+
 end
+
+
+# create_table "authors", force: :cascade do |t|
+#   t.string   "name"
+#   t.string   "email"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
